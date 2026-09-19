5ead6308bc9353f364af9785cd6ef43808b1a9d1
Thiago Lugli <thiagoelg@gmail.com>
2026-09-19 00:04:25 -0300

    enable write

---
+++ assets/ellipsis-vertical-D45cY1FP.js
@@ -0,0 +1,1 @@
+import{T as e}from"./resources-D5MYjAv2.js";var t={name:`ellipsis-vertical`,size:24,node:[[`circle`,{cx:`12`,cy:`12`,r:`1`,key:`41hilf`}],[`circle`,{cx:`12`,cy:`5`,r:`1`,key:`gxeob9`}],[`circle`,{cx:`12`,cy:`19`,r:`1`,key:`lyex9k`}]],aliases:[`more-vertical`]};t.node;var n=e(t);export{n as t};
