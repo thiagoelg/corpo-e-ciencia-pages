f07ad1f528584180f7974aa51edb6e20e685da20
Thiago Lugli <thiagoelg@gmail.com>
2026-09-23 13:33:56 -0300

    Restore /corpo-e-ciencia-pages/ base path

---
+++ assets/book-open-ICSmefTJ.js
@@ -0,0 +1,1 @@
+import{T as e}from"./resources-D5MYjAv2.js";var t={name:`book-open`,size:24,node:[[`path`,{d:`M12 5v16`,key:`1f6ucr`}],[`path`,{d:`M20.001 19A2 2 0 0022 17V5a2 2 0 00-1.999-2L16 3.002A5 5 0 0012 5a5 5 0 00-4-2H4a2 2 0 00-2 2v12a2 2 0 001.999 2H8a5 5 0 014 2 5 5 0 014-2z`,key:`1fyvmf`}]]};t.node;var n=e(t);export{n as t};
--- assets/skeleton-bGuUUB1f.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-D3jfb0Ew.js";import{b as t}from"./resources-Cul4pMZb.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{"data-slot":`skeleton`,className:t(`animate-pulse rounded-md bg-muted`,e),...r})}export{r as t};
